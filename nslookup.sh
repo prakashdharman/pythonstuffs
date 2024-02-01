38494eb26d29ec00f65f352cce47584df62a7e89
prakashdharman <36411283+prakashdharman@users.noreply.github.com>
2024-02-01 16:44:57 +0100

    Update nslookup.sh

--- nslookup.sh
+++ nslookup.sh
@@ -15,11 +15,10 @@ if [ ! -f "$filename" ]; then
 fi
 
 # Read the file line by line
-while IFS= read -r line; do
-    # Extract IP and hostname from each line
-    ip=$(echo "$line" | awk '{print $1}')
-    hostname=$(echo "$line" | awk '{print $2}')
+while IFS= read -r hostname; do
+    # Get IP address corresponding to the hostname
+    ip=$(host "$hostname" | awk '/has address/ {print $4}')
     
     # Print IP and hostname
-    echo "IP: $ip, Hostname: $hostname"
+    echo "$ip $hostname"
 done < "$filename"
