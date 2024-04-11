28c974d2e61628f01e6a1282f0b6eccaea01f5ca
prakashdharman <36411283+prakashdharman@users.noreply.github.com>
2024-04-11 16:12:03 +0200

    Update logger_tcp.py

--- logger_tcp.py
+++ logger_tcp.py
@@ -1,35 +1,44 @@
 import logging
-import logging.handlers
 import socket
-import ssl
 import time
+
+def log_to_tcp(host, port, message):
+    try:
+        # Create a TCP socket
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # Connect to the TCP listener
+        tcp_socket.connect((host, port))
+        
+        # Send the log message over the socket
+        tcp_socket.sendall(message.encode())
+        
+        # Close the TCP socket
+        tcp_socket.close()
+    except Exception as e:
+        print("Error while logging:", e)
 
 # Configure logging
 logger = logging.getLogger("my_logger")
 logger.setLevel(logging.INFO)
 
-# Create a TCP socket
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Wrap the socket with SSL/TLS
-ssl_socket = ssl.wrap_socket(tcp_socket, ssl_version=ssl.PROTOCOL_TLS)
-
-# Connect to the TCP listener
-ssl_socket.connect(('localhost', 9999))
-
-# Create a socket handler with the SSL socket
-tcp_handler = logging.StreamHandler(ssl_socket)
+# Create a TCP handler
+host = 'localhost'
+port = 9999
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-tcp_handler.setFormatter(formatter)
 
-# Add the TCP handler to the logger
+def log_tcp(message):
+    log_to_tcp(host, port, message)
+
+tcp_handler = logging.StreamHandler()
+tcp_handler.emit = log_tcp
+tcp_handler.setFormatter(formatter)
 logger.addHandler(tcp_handler)
 
 # Create a console handler
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
-
-# Add the console handler to the logger
+console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 # Log some messages
@@ -37,5 +46,5 @@ for i in range(5):
     logger.info("This is log message %d" % i)
     time.sleep(1)  # Sleep for 1 second between log messages
 
-# Close the SSL socket
-ssl_socket.close()
+# Close the TCP handler (there's no need to close console handler)
+logger.removeHandler(tcp_handler)
